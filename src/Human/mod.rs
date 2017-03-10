3c1b1caa35143cfd6e8053fc862bd3f5e67b07bd
hogehoge1298 <hogehoge1298@gmail.com>
2017-03-10 15:44:37 +0900

    クラスライクな部分がなんとなく理解

--- src/Human/mod.rs
+++ src/Human/mod.rs
@@ -13,10 +13,12 @@ impl Human {
         &self.name
     }
 
-    pub fn entryInZeroTime() {}
-
     pub fn apprivoiser(&mut self) {
         println!("アプリポワゼ！ 颯爽登場！銀河美少年!タウバーン！");
         self.name = "銀河美少年".to_string();
+    }
+
+    pub fn input_action(&mut self, args_line: String) {
+        println!("{}", args_line);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,39 @@
 extern crate RPGBattle;
 
+use std::thread;
+use std::io;
+use std::time::Duration;
 use RPGBattle::Human::Human;
+
+pub fn clear_screen() -> bool {
+    std::process::Command::new("clear").status().unwrap().success()
+}
 
 
 fn main() {
     let mut p1 = Human::new("ツナシ・タクト");
 
-    println!("{}", p1.get_name());
-    p1.apprivoiser();
-    println!("{}", p1.get_name());
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Faild to read line");
+
+    p1.input_action(guess);
+
+    /*if clear_screen() {
+        println!("{}", p1.get_name());
+    }
+
+    thread::sleep(Duration::from_millis(1000));
+
+    if clear_screen() {
+        p1.apprivoiser();
+    }
+
+    thread::sleep(Duration::from_millis(1000));
+
+    if clear_screen() {
+        println!("{}", p1.get_name());
+    }*/
+
 
 }
